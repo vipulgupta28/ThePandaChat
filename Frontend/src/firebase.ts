e2808cf95ad25eb3c48c7cae3796a3a0e6ceaf81
Vipul Gupta <121814604+vipulgupta28@users.noreply.github.com>
2025-06-27 10:03:51 +0530

    firebase.ts

--- Frontend/src/firebase.ts
+++ Frontend/src/firebase.ts
@@ -3,8 +3,7 @@ import {initializeApp} from 'firebase/app'
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA8ASzCirm3KzhG5HMpkoxFLYsO2vTjQfs",
-  authDomain: "panda-8fedc.firebaseapp.com",
+ 
   // other config values
 };
 
